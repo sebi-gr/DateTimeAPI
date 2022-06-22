1963d245c3b493ef71777a1e41c89091442868a1
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2022-06-22 18:42:03 +0200

    Created REST for date request

--- src/main/java/com/example/DateTimeAPI/controller/DateTimeController.java
+++ src/main/java/com/example/DateTimeAPI/controller/DateTimeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -26,5 +27,10 @@ public class DateTimeController {
     @GetMapping("/date")
     public ResponseEntity<LocalDate> getDate () {
         return new ResponseEntity<>(dateTimeService.getDate(), HttpStatus.OK);
+    }
+
+    @GetMapping("/dateRequest")
+    public ResponseEntity<String> getDateRequest (@RequestParam String format) {
+        return new ResponseEntity<>(dateTimeService.getDateRequest(format), HttpStatus.OK);
     }
 }
